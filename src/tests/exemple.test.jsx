ce8f4490a9dda6c00f6b9110b1dad21391f4367a
Thomas Deneux <thomas.deneuxrahil@gmail.com>
2023-10-21 18:52:32 +0200

    test unitaire non fonctionnel test

--- src/tests/exemple.test.jsx
+++ src/tests/exemple.test.jsx
@@ -3,6 +3,6 @@ import {describe, expect, it} from "vitest";
 describe ("Exemple nom section test", () => {
     it ("Exemple test n°1", () => {
         const sum = 2 + 2;
-        expect(sum).toEqual(4);
+        expect(sum).toEqual(5);
     })
 })
