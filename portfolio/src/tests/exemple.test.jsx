0bcfabc845b7c587bec7662c10248d70bb756676
Thomas Deneux <thomas.deneuxrahil@gmail.com>
2023-10-21 15:27:32 +0200

    Mise en place Archi React Vite VItest

---
+++ portfolio/src/tests/exemple.test.jsx
@@ -0,0 +1,8 @@
+import {describe, expect, it} from "vitest";
+
+describe ("Exemple nom section test", () => {
+    it ("Exemple test n°1", () => {
+        const sum = 2 + 2;
+        expect(sum).toEqual(4);
+    })
+})
