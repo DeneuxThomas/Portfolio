002ec28c771eefb77e4837410bce4f0b433e34a0
Thomas Deneux <thomas.deneuxrahil@gmail.com>
2023-10-22 15:24:26 +0200

    format file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import './scss/App.scss'
+import "./scss/App.scss";
 
 function App() {
-
   return (
     <>
-      <h1 id='titre-h1'>TOTO</h1>
+      <h1 id="titre-h1">TOTO</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
